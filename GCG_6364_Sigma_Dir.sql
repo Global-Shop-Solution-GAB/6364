3dcc273c7a6f556b2cb209896efdb3130dd986af
Service Web <sw@gssmail.com>
2024-03-18 14:50:11 -0500

    Commit changes in C:\Websites\Git\emarlow\6364\GCG_6364_Sigma_Dir.sql

---
+++ GCG_6364_Sigma_Dir.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "GCG_6364_Sigma_Dir"(
+ "Company" CHAR(3),
+ "Sig_Provider" CHAR(50),
+ "Sig_Server" CHAR(50),
+ "Sig_Data" CHAR(50),
+ "UID" CHAR(20),
+ "PWD" CHAR(20),
+ "Input_Dir" LONGVARCHAR,
+ "Complete_Dir" LONGVARCHAR,
+ "Bom_Dir" LONGVARCHAR,
+ "Remnants_Dir" LONGVARCHAR,
+ "Image_Dir" LONGVARCHAR);
